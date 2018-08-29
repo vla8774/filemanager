be6a532e6654f0b2623d2f99e693bb0e44f50bf1
Кузнецов Владимир Вячеславович <F0621037@RG62.LOCAL>
2018-08-29 11:10:34 +0300

    v.1.2

--- file/views.py
+++ file/views.py
@@ -5,16 +5,22 @@ from .models import FilePost, SubjectFiles
 def post_file(request):
     posts = FilePost.objects.all()
     return render(request, 'file/post_file.html', {'posts': posts})
-
+    get_base_menu()
 
 def subject_detail(request, pk):
-    posts = get_object_or_404(SubjectFiles, pk=pk)
-    return render(request, 'file/subject_file.html', {'posts': posts})
+    post = get_object_or_404(SubjectFiles, pk=pk)
+    return render(request, 'file/subject_file.html', {'posts': post})
+
+
+def get_base_menu(request):
+    post = SubjectFiles.objects.all()
+    return render(request, 'blog/base_menu.html', {'menu': post})
 
 
 def subject_all(request):
-    posts = SubjectFiles.objects.all()
-    return render(request, 'file/subject_all.html', {'posts': posts})
+    subject = SubjectFiles.objects.all()
+    return render(request, 'file/subject_all.html', {'subject': subject})
+
 
 
 '''def subject_all(request):
